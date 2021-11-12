548b5cd1b2051a80eeb3e0b8e877152e282ab58c
tatsw10 <58251752+tatsw10@users.noreply.github.com>
2021-11-12 20:10:36 +0000

    final code

--- src/app.js
+++
@@ -1,6 +0,0 @@
-const feedDisplay = document.querySelector('#feed')
-
-
-fetch('https://animereleases.herokuapp.com/anime')
-    .then(response => response.json())
-    .then(data => console.log(data))
